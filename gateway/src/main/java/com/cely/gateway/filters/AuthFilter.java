e9c316a173c182435a9946baa3a167e67dcb8f76
William Cely <wmerardocely@gmail.com>
2026-04-02 13:43:55 -0300

    auth2

---
+++ gateway/src/main/java/com/cely/gateway/filters/AuthFilter.java
@@ -0,0 +1,59 @@
+package com.cely.gateway.filters;
+
+import com.cely.gateway.dtos.TokenDto;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class AuthFilter implements GatewayFilter {
+
+    private final WebClient webClient;
+    private static final String AUTH_VALIDATE_URI = "http://localhost:3031/auth-server/auth/jwt";
+    private static final String ACCESS_TOKEN_HEADER_NAME = "accessToken";
+
+    public AuthFilter(){
+        this.webClient = WebClient.builder().build();
+    }
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        if (!exchange.getRequest().getHeaders().containsHeader(HttpHeaders.AUTHORIZATION)) {
+            return this.onError(exchange);
+        }
+
+        final var tokenHeder = exchange
+                .getRequest()
+                .getHeaders()
+                .get(HttpHeaders.AUTHORIZATION).get(0);
+
+        final  var chunks = tokenHeder.split(" ");
+
+        if (chunks.length != 2 || !chunks[0].equals("Bearer")) {
+            return this.onError(exchange);
+        }
+
+        final var token = chunks[1];
+
+        return webClient
+                .post()
+                .uri(AUTH_VALIDATE_URI)
+                .header(ACCESS_TOKEN_HEADER_NAME, token)
+                .retrieve()
+                .bodyToMono(TokenDto.class)
+                .map(response->exchange)
+                .flatMap(chain::filter);
+    }
+
+    private Mono<Void> onError(ServerWebExchange exchange) {
+        final ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(HttpStatus.BAD_REQUEST);
+        return response.setComplete();
+    }
+}
